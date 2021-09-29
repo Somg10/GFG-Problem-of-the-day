0eae2b86cd973fe0266da7c9c7a25b7383bea4c9
Som Kr. Gupta <somgupta004@gmail.com>
2021-09-29 09:16:10 +0530

    Create Restricted Pacman.py

---
+++ Restricted Pacman.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+
+class Solution: 
+    def candies(self, m,n): 
+        # Your code goes here 
+        c = ((m-1)*(n-1)//2)
+        return c
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+	
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+    	arr = list(map(int, input().strip().split()))
+    	m = arr[0]
+    	n = arr[1]
+    	obj = Solution()
+    	print(obj.candies(m,n)) 
+
+
+
+# } Driver Code Ends
