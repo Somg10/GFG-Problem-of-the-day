e4942fece571be954ded99128ba49fbc229974b6
Som Kr. Gupta <somgupta004@gmail.com>
2021-10-15 23:18:51 +0530

    Create All Unique Permutations of an array.py

---
+++ All Unique Permutations of an array.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+
+from itertools import permutations as per
+class Solution:
+    def uniquePerms(self, arr, n):
+        # code here 
+        ans = []
+        res = set(per(arr))
+        for i in res:
+            ans.append(i)
+        return sorted(ans)
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        n=int(input())
+        arr=list(map(int,input().split()))
+        
+        ob = Solution()
+        res = ob.uniquePerms(arr,n)
+        for i in range(len(res)):
+            for j in range(n):
+                print(res[i][j],end=" ")
+            print()
+# } Driver Code Ends
