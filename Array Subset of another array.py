fe0657504a84e1e7309ab4beeec42754de5d24d8
Som Kr. Gupta <somgupta004@gmail.com>
2021-11-05 21:49:59 +0530

    Create Array Subset of another array.py

---
+++ Array Subset of another array.py
@@ -0,0 +1,38 @@
+#User function Template for python3
+
+def isSubset( a1, a2, n, m):
+    a1 = set(a1)
+    a2 = set(a2)
+    if a2.issubset(a1):
+        return "Yes"
+    else:
+        return "No"
+    
+    
+    
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        sz = [int(x) for x in input().strip().split()]
+        n, m = sz[0], sz[1]
+        a1 = [int(x) for x in input().strip().split()]
+        a2 = [int(x) for x in input().strip().split()]
+        
+        print(isSubset( a1, a2, n, m))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
